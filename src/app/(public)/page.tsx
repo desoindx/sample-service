d7e4b09a02f824632cffa3b931abd11450b6e4f1
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2024-08-15 14:53:50 +0200

    feat: change home text

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,3 +1,7 @@
 export default async function Home() {
-  return <div>Nothing here</div>;
+  return (
+    <div className="fr-container fr-container--fluid">
+      <h1>Bienvenue sur mon fake service</h1>
+    </div>
+  );
 }
