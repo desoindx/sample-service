888753bf870e1883ec3f5bff1c38a467fed99f30
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2024-08-27 14:54:55 +0200

    fix: ownerId

--- src/services/project1.ts
+++ src/services/project1.ts
@@ -8,14 +8,14 @@ export const createProject1 = async (id: string) => {
   const hubData = await axios.get<{
     name: string;
     description: string;
-    owner: string;
+    ownerUserId: string;
   }>(`${process.env.HUB_URL}/api/projects/${id}`);
   return prisma.project1.create({
     data: {
       id,
       name: hubData.data.name,
       description: hubData.data.description,
-      owner: hubData.data.owner,
+      owner: hubData.data.ownerUserId,
       score1: Math.random() * 20,
       score2: Math.random() * 20,
       scoreTotal: Math.random() * 20,
